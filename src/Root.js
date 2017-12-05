26e3c66f6f53b615fee7d4c7ed9730d5f819944a
Gianluca Esposito <g@esposi.to>
2017-12-05 18:58:21 +0100

    Add React router

--- src/Root.js
+++ src/Root.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Router, Switch, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+
 import AppPage from './components/AppPage';
+
+const history = createBrowserHistory();
 
 // use this component as it was the entry point of your dev server
 
 const Root = () => {
   return (
-    <AppPage />
+    <Router history={history}>
+      <Switch>
+        <Route exact={true} path='/' component={AppPage} />
+      </Switch>
+    </Router>
   );
 }
 
